48ed32c0840a376295edc2ca33cc6fa5b3b85dcc
Stanislav Kostenko <stanislav_kostenko@epam.com>
2020-03-11 19:35:08 +0200

    [EPMDEDP-4792] Add validation on startVersioningFrom field during app creation
    Change-Id: I2d6066aa786b92f950e54c1c655f665206856d66

--- static/js/codebase-branch-validation.js
+++ static/js/codebase-branch-validation.js
@@ -164,18 +164,6 @@ function isHashCommitValid() {
     }
 }
 
-function isBranchVersionValid(branchVersion) {
-    if (branchVersion.val().length === 0) {
-        return false;
-    } else {
-        return !(!branchVersion.val() || !checkBranchVersion(branchVersion.val()));
-    }
-}
-
-function checkBranchVersion(branchVersion) {
-    return /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|(?!.*RC|.*GA|.*SNAPSHOT)\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/i.test(branchVersion)
-}
-
 function checkBranchName(branchName) {
     return /^[a-z0-9][a-z0-9-.]*[a-z0-9]$/.test(branchName);
 }
@@ -194,19 +182,6 @@ function handleBranchNameValidation() {
         $('.invalid-feedback.branch-name').hide();
     }
     return isBranchValid;
-}
-
-function handleBranchVersionValidation(branchVersion) {
-    let isValid = isBranchVersionValid(branchVersion),
-        elClass = branchVersion.attr("id");
-    if (!isValid) {
-        branchVersion.addClass('non-valid-input');
-        $('.invalid-feedback.' + elClass).show();
-    } else {
-        branchVersion.removeClass('non-valid-input');
-        $('.invalid-feedback.' + elClass).hide();
-    }
-    return isValid;
 }
 
 function handleCommitHashValidation() {
