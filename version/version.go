c0c910c314f81510b2b6eb83a2a805f54cc5edd6
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-07-09 18:16:28 +0300

    [EPMDEDP-5365] Fix delete from overview
    Change-Id: Id3ed95fa12f5c99e817db915134d2b1bd3665a0a
    (cherry picked from commit bac6153c73dfce27cc2368b4d9c2167c86401d52)
    (cherry picked from commit 72fc8d4838fa49e42ef7b7c264bc28593b9792c4)

--- version/version.go
+++ version/version.go
@@ -1,5 +1,9 @@
 package version
 
 var (
+<<<<<<< HEAD   (1a1a51 [DOC] Add AC guide of v.2.5.0 - part 2)
 	Version = "2.5.0"
+=======
+	Version = "2.4.1"
+>>>>>>> CHANGE (72fc8d [EPMDEDP-5365] Fix delete from overview)
 )
