2bbe0329d2169a3f8ccdb1073042fb10392585b2
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-07-09 18:16:28 +0300

    [EPMDEDP-5365] Fix delete from overview
    Change-Id: Id3ed95fa12f5c99e817db915134d2b1bd3665a0a
    (cherry picked from commit bac6153c73dfce27cc2368b4d9c2167c86401d52)

--- version/version.go
+++ version/version.go
@@ -1,5 +1,9 @@
 package version
 
 var (
+<<<<<<< HEAD   (ed1b38 [EPMDEDP-5365] Fix redirect with base path)
 	Version = "2.4.0"
+=======
+	Version = "2.3.2"
+>>>>>>> CHANGE (bac615 [EPMDEDP-5365] Fix delete from overview)
 )
