66ee94bbd9e8892f82a74e786d2802fb66b5bb40
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-11-06 10:17:05 +0200

    [EPMDEDP-5699]: Add unit tests
    Change-Id: Iadf199819e8e33fb0068455f6f409b5c63406c26

--- service/codebase_test.go
+++ service/codebase_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	"edp-admin-console/models/query"
 	"edp-admin-console/repository/mock"
+	"edp-admin-console/service/perfboard"
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -34,4 +35,93 @@ func TestGetCodebaseByNameMethod_ShouldBeExecutedWithError(t *testing.T) {
 	c, err := cs.GetCodebaseByName("stub-name")
 	assert.Error(t, err)
 	assert.Nil(t, c)
+}
+
+func TestGetCodebaseByNameMethod_WithPerfServerShouldBeExecutedSuccessfully(t *testing.T) {
+	mCodebase := new(mock.MockCodebase)
+	pbrm := new(mock.MockPerfBoard)
+	cs := CodebaseService{
+		ICodebaseRepository: mCodebase,
+		PerfService: perfboard.PerfBoard{
+			PerfRepo: pbrm,
+		},
+	}
+
+	mCodebase.On("GetCodebaseByName", "stub-name").Return(
+		query.Codebase{
+			Id:           1,
+			PerfServerId: getIntP(1),
+			Perf:         nil,
+		}, nil)
+
+	pbrm.On("GetPerfServerName", 1).Return(
+		&query.PerfServer{
+			Id:   1,
+			Name: "fake-server",
+		}, nil)
+
+	pbrm.On("GetCodebaseDataSources", 1).Return(
+		[]string{"ds1"}, nil)
+
+	c, err := cs.GetCodebaseByName("stub-name")
+	assert.NoError(t, err)
+	assert.NotNil(t, c)
+}
+
+func TestGetCodebaseByNameMethod_WithPerfServerPerfServerShouldNotBeFound(t *testing.T) {
+	mCodebase := new(mock.MockCodebase)
+	pbrm := new(mock.MockPerfBoard)
+	cs := CodebaseService{
+		ICodebaseRepository: mCodebase,
+		PerfService: perfboard.PerfBoard{
+			PerfRepo: pbrm,
+		},
+	}
+
+	mCodebase.On("GetCodebaseByName", "stub-name").Return(
+		query.Codebase{
+			Id:           1,
+			PerfServerId: getIntP(1),
+			Perf:         nil,
+		}, nil)
+
+	pbrm.On("GetPerfServerName", 1).Return(nil, errors.New("failed"))
+
+	c, err := cs.GetCodebaseByName("stub-name")
+	assert.Error(t, err)
+	assert.Nil(t, c)
+}
+
+func TestGetCodebaseByNameMethod_WithPerfServerDataSourceShouldNotBeFound(t *testing.T) {
+	mCodebase := new(mock.MockCodebase)
+	pbrm := new(mock.MockPerfBoard)
+	cs := CodebaseService{
+		ICodebaseRepository: mCodebase,
+		PerfService: perfboard.PerfBoard{
+			PerfRepo: pbrm,
+		},
+	}
+
+	mCodebase.On("GetCodebaseByName", "stub-name").Return(
+		query.Codebase{
+			Id:           1,
+			PerfServerId: getIntP(1),
+			Perf:         nil,
+		}, nil)
+
+	pbrm.On("GetPerfServerName", 1).Return(
+		&query.PerfServer{
+			Id:   1,
+			Name: "fake-server",
+		}, nil)
+
+	pbrm.On("GetCodebaseDataSources", 1).Return(nil, errors.New("failed"))
+
+	c, err := cs.GetCodebaseByName("stub-name")
+	assert.Error(t, err)
+	assert.Nil(t, c)
+}
+
+func getIntP(val int) *int {
+	return &val
 }
